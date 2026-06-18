3940d227b60ffd63a08b21de56ebc58bb5e8bcaa
fionajade <jadee.fiona4@gmail.com>
2026-06-18 21:26:59 +0800

    fixed incident

--- admin/incident.php
+++ admin/incident.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report Incident - TAALERT</title>
+    <title>Report Incident</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
